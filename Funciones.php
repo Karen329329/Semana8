7a2500588ccf523411cc1d5e67a8fc2ebbd31c84
Karen Mancilla <karen.mancillasilva@gmail.com>
2024-07-29 00:34:33 -0400

    carga de archivos agencia de viajes

---
+++ Funciones.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class filtro
+{
+	public function __construct()
+	{
+		require_once "datos.php";
+	}
+	public function filtrardestino($pais, $fecha)
+	{
+		$objDatos = new Hotel_Vuelo();
+		$retorno = $objDatos->mostrarHoteles($pais, $fecha);
+		return $retorno;
+	}
+	public function guardar_registro($hotel, $pais, $ciudad, $fecha_viaje, $duracion)
+	{
+
+	}
+}
+?>
